9b20a7cdedd9a9e38b32550c3dbdb1d22b6a68a3
Gerald <geraldjoly191@gmail.com>
2024-09-09 23:30:01 -0300

    Implementé la clase CPersona.csCChofer.cs

---
+++ CChofer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergencia_Medica
+{
+    internal class CChofer:CPersona
+    {
+
+        string codigo;
+        uint numero_de_Licencia;
+        string distrito;
+
+        public CChofer(string nombre,string apellido, string codigo, uint numero_de_Licencia, string distrito):base(nombre,apellido)
+        {
+            this.codigo = codigo;
+            this.numero_de_Licencia = numero_de_Licencia;
+            this.distrito = distrito;
+        }
+
+
+
+        public override string ToString()
+        {
+            return base.ToString() +"\n Codigo :"+ this.codigo +"\n N° de Licencia :"+ this.numero_de_Licencia +"\n Distrito :" + this.distrito;
+        }
+    }
+}
