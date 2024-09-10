a88b7dc35e9fb1337c16bcefdbd7753d105f1f9e
Gerald <geraldjoly191@gmail.com>
2024-09-09 23:31:17 -0300

    Implementé la clase ClProfecionales.cs

---
+++ ClProfecionales.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergencia_Medica
+{
+    enum CATEGOTIA_PROFECIONAL
+    {
+        medico,
+        enfermero,
+        paramedico
+    }
+    internal class CiProfecionales:CPersona
+    {
+        string matricula;
+        uint codigo;
+        Enum CATEGOTIA_PROFECIONAL ;
+
+        public CiProfecionales(string nombre, string apellido,string matricula, uint codigo, Enum cATEGOTIA_PROFECIONAL):base(nombre,apellido)
+        {
+            this.matricula = matricula;
+            this.codigo = codigo;
+            CATEGOTIA_PROFECIONAL = cATEGOTIA_PROFECIONAL;
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n Matricula :"+ this.matricula + "\n Codigo :"+ this.codigo +"\n Categoria Profecional :"+ this.CATEGOTIA_PROFECIONAL.ToString();
+        }
+    }
+}
