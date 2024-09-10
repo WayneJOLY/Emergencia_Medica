70dc955b5a9a5b0b18b00b2a4ae5eb4562a4955e
Gerald <geraldjoly191@gmail.com>
2024-09-09 23:28:58 -0300

    Implementé la clase CPersona.cs

---
+++ CPersona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergencia_Medica
+{
+    internal class CPersona
+    {
+        string nombre;
+        string apellido;
+
+        public CPersona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public string Nombre { get {  return nombre; } }
+
+        public override string ToString()
+        {
+            return "\n Nombre :"+ this.nombre+"\n Apellido :" +this.apellido;
+        }
+    }
+}
