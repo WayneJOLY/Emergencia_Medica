42910ea0bb563d4148936b1be019948c81681c5c
Gerald <geraldjoly191@gmail.com>
2024-09-09 23:32:17 -0300

    Implementé la clase CVehiculo.cs

---
+++ CVehiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergencia_Medica
+{
+    internal class CVehiculo
+    {
+        private string patente;
+        private string marca;
+        private string modelo;
+
+        public CVehiculo(string patente, string marca, string modelo)
+        {
+            this.patente = patente;
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+
+        public override string ToString()
+        {
+            return "\n Patente :"+ this.patente + "\n Marca :"+ this.marca +"\n Modelo :"+ this.modelo;
+        }
+    }
+}
