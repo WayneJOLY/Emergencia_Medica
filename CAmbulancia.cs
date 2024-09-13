09dc6b8dea7c74ea59f41659e9b2ccf86481ec4a
Gerald <geraldjoly191@gmail.com>
2024-09-13 17:13:33 -0300

    Implementé la clase CAmbulancia

---
+++ CAmbulancia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergencia_Medica
+{
+    public enum TipoDeAmbulancia
+    {
+        emergencia ,
+        terapia_intensiva_móvil,
+        unidad_coronaria_móvil,
+    }
+    internal class CAmbulancia:CVehiculo
+    {
+        private TipoDeAmbulancia tipoDeAmbulancia;
+
+        CAmbulancia(string patente, string marca, string modelo,uint tipoAmbulancia) : base(patente, marca, modelo)
+        {
+            this.tipoDeAmbulancia = (TipoDeAmbulancia)tipoAmbulancia;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()+ "\n Tipo de Ambulancia :" +this.tipoDeAmbulancia.ToString();
+
+        }
+    }
+}
