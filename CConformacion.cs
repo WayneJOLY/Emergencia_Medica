30d840c146b8e3ef33c2dd79b9eeaed727aa31d0
Gerald <geraldjoly191@gmail.com>
2024-09-13 20:55:08 -0300

    Implementé la clase CConformacion.cs

---
+++ CConformacion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergencia_Medica
+{
+    public class CConformacion
+    {
+        DateOnly fecha;
+        CChofer chofer;
+        ArrayList listaDeIntergrantes;
+        bool esApto;
+
+        CConformacion(DateOnly fecha, CChofer chofer, ArrayList listaDeIntergrantes)
+        {
+            this.fecha = fecha;
+            this.chofer = chofer;
+            this.listaDeIntergrantes = listaDeIntergrantes;
+        }
+
+        public bool AgregarIntegrante(CPersona persona)
+        {
+            listaDeIntergrantes.Add(persona);
+            return true;
+        }
+        
+       
+        public bool getEsApto()
+        {
+            return esApto;
+        }
+
+        public void setEsApto(bool esApto)
+        {
+            this.esApto = esApto;
+        } 
+        
+        public override string ToString()
+        {
+            string datos = "";
+
+            foreach(CPersona persona in listaDeIntergrantes)
+            {
+                datos += persona.ToString();
+            }
+
+            return "\n Fecha :" +fecha.ToString() +"\n Chofer :" + this.chofer.ToString() + "\n Lista de Integrantes :" + datos;
+        }
+
+    }
+}
